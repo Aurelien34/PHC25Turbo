f50fb65b4cd1d3fca7810423dbf3518b1c5bb6f8
Aurelien34 <aurelien.brioude@gmail.com>
2025-08-15 10:25:22 +0200

    Moved todo list to a todo file!

--- main.s
+++ main.s
@@ -8,21 +8,6 @@
     global emulator_security_idle
     endif
 
-    ; sound ?
-    ; nouveaux circuits enchaînes
-    ; détection des tours
-    ; affichage de stats (tours restants, temps?)
-    ; taches d'huile
-    ; profils de voitures (vitesse de rotation, profil d'accélération, dérapages)
-    ; dérapage en fonction de la vitesse?
-    ; photos des voitures
-    ; images en niveaux de gris
-    ; collisions entre voitures
-    ; fun échappement?
-    ; faire arriver la voiture par le côté?
-    ; Compression RLE
-    ; faire marcher les joysticks!
-
 players_count:
     dc.b 0
 
