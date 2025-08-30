5f367fd64f50d7ec17fcf075a6d365970d75ec1f
Aurelien34 <aurelien.brioude@gmail.com>
2025-08-30 22:26:47 +0200

    Remove blinking grayscale car / Added rudimentary  circuit miniature generation

--- circuit_picker.s
+++ circuit_picker.s
@@ -8,9 +8,10 @@
     global circuit_picker_show
 
 CURSOR_VRAM_ADDRESS equ 1+80*32+VRAM_ADDRESS
-IMAGE_VRAM_ADDRESS equ 19+20*32+VRAM_ADDRESS
-IMAGE_BYTE_WIDTH equ 8
-IMAGE_HEIGHT equ 28
+
+CIRCUIT_VRAM_ADDRESS equ 19+20*32+VRAM_ADDRESS
+CIRCUIT_WIDTH equ 16
+CIRCUIT_HEIGHT equ 12
 
 circuits_list:
     dc.w rlh_circuit_monaco
@@ -58,9 +59,6 @@ circuit_picker_circuit_index:
 previous_input_value:
     dc.b 0
 
-image_counter:
-    dc.b 0
-
 circuit_picker_show:
     ld a,$ff
     call clear_screen
@@ -69,11 +67,6 @@ circuit_picker_show:
     ; decompress round borders
     ld hl,rlh_round_borders
     ld de,RAM_MAP_DECOMPRESSION_BUFFER_32
-    call decompress_rlh
-
-    ; decompress grayscale image
-    ld hl,rlh_gs_car
-    ld de,RAM_MAP_PRECALC_VEHICLE_1
     call decompress_rlh
 
     ; draw rectangles
@@ -105,6 +98,8 @@ circuit_picker_show:
     ; Show cursor for current position
     call show_cursor
 
+    ; Draw current circuit miniature
+    call select_circuit
 .loop
 
     ; Wait for VBL
@@ -113,9 +108,6 @@ circuit_picker_show:
     ; Black on green
     ld a,%10110110
     out ($40),a
-
-    ; Update grayscale image
-    call update_grayscale_image
 
     ; Read inputs
     ld a,(previous_input_value)
@@ -136,6 +128,7 @@ circuit_picker_show:
     dec a
     ld (circuit_picker_circuit_index),a
     call show_cursor
+    call select_circuit
     jr .inputs_end
 .not_left:
     bit INPUT_BIT_RIGHT,a
@@ -148,12 +141,13 @@ circuit_picker_show:
     inc a
     ld (circuit_picker_circuit_index),a
     call show_cursor
+    call select_circuit
     jr .inputs_end
 .not_right:
     bit INPUT_BIT_FIRE,a
     jp z,.not_fire
     ; Fire
-    call select_circuit
+    ;call select_circuit
     ret
 .not_fire:
 .inputs_end:
@@ -181,6 +175,7 @@ select_circuit:
     inc hl
     ld b,(hl)
     ld (circuit_picker_circuit_data_address),bc
+    call draw_circuit_miniature
     ret
 
 ; Cursor position in [a]
@@ -300,43 +295,132 @@ draw_black_rectangle:
 
     ret
 
-update_grayscale_image:
-    ; Determine image to be drawn
-    ; increment image counter
-    ld hl,image_counter
-    ld a,(hl)
-    inc a
-    cp 3
-    jr nz,.not_3:
-    xor a
-.not_3
-    ld (hl),a
-    or a
-    jp nz,.image_2
-    ld hl,RAM_MAP_PRECALC_VEHICLE_1+IMAGE_BYTE_WIDTH*IMAGE_HEIGHT
-    ld de,IMAGE_VRAM_ADDRESS
-    call draw_half_gs_image
-    jr .image_done
-.image_2:
-    ld hl,RAM_MAP_PRECALC_VEHICLE_1
-    ld de,IMAGE_VRAM_ADDRESS
-    call draw_half_gs_image
-.image_done
-    ld a,(image_counter)
-    cpl
-    ld (VRAM_ADDRESS+32*192-1),a; Force 60Hz refresh in MAME!
-    ret
+    dc.b "                    BREAKPOINT1                     "
+draw_circuit_miniature:
+    ; top right rectangle (todo: remove this!)
+    ld hl,18+11*32+VRAM_ADDRESS
+    ld ix,10+54<<8
+    call draw_black_rectangle
 
-draw_half_gs_image:
-    ld ixh,IMAGE_HEIGHT
-.yloop:
-    ld bc,IMAGE_BYTE_WIDTH
-    ldir
-    ld bc,32-IMAGE_BYTE_WIDTH
+    ; load the circuit
+    ld hl,(circuit_picker_circuit_data_address)
+    call load_circuit
+
+    ; initialize circuit and screen pointers
+    ld hl,RAM_MAP_CIRCUIT_DATA
+    ld de,CIRCUIT_VRAM_ADDRESS
+
+    ; loop on rows
+    ld iyl,CIRCUIT_HEIGHT
+.loopy:
+    ; loop on groups of columns
+    ld ixl,0
+.loopx:
+    ; load circuit tile info
+    ld b,0
+    ld c,(hl)
+    srl c
+    srl c
+    srl c
+    push hl
+    ld hl,minitiles_data
+    add hl,bc
+    ld a,(hl)
+    pop hl
+    call draw_tile
+    
+    inc hl
+    inc ixl
+    ld a,ixl
+    and %11
+    jp nz,.continue
+    inc de ; x is a multiple of 4, move to next 8 bits area
+.continue
+    ld a,ixl
+    cp CIRCUIT_WIDTH
+    jp nz,.loopx
     ex de,hl
+    ld b,0
+    ld c,32*2-4
     add hl,bc
     ex de,hl
-    dec ixh
-    jr nz,.yloop
-
+    dec iyl
+    jp nz,.loopy
     ret
+
+
+; [a] = tile data
+; [de] = mem destination
+; [ixl] = x
+; [iyl] = y
+    dc.b "                    BREAKPOINT2                     "
+draw_tile:
+    push hl
+    push bc
+    push de
+    
+    ld c,a ; backup tile data
+
+    ld a,ixl
+    inc a
+    neg
+    and %11
+    ld b,a ; b contains shift count
+
+    ld a,c
+    call .common_processing
+
+    push bc
+    ex de,hl
+    ld bc,32
+    add hl,bc
+    pop bc
+    ex de,hl
+
+    ld a,ixl
+    inc a
+    neg
+    and %11
+    ld b,a ; b contains shift count
+
+    ld a,c
+    srl a
+    srl a
+    call .common_processing
+
+    pop de
+    pop bc
+    pop hl
+    ret
+
+.common_processing:
+    and %11 ; keep top row data
+.shiftloop:
+    dec b
+    jp m,.shiftloop_end
+    add a
+    add a
+    jr .shiftloop
+.shiftloop_end:
+    ld b,a
+    ld a,(de)
+    or b
+    ld (de),a
+    ret
+
+minitiles_data:
+    ; 1 first bits are image data for even rows. 2 next bits are for odd rows. 4 next bits are unused :(
+    dc.b %1111 ; empty
+    dc.b %0000 ; full
+    dc.b %0000 ; vertical
+    dc.b %0000 ; horizontal
+    dc.b %0100 ; angle top-left
+    dc.b %1000 ; angle top-right
+    dc.b %0010 ; angle bottom-right
+    dc.b %0001 ; angle bottom-left
+    dc.b %0101 ; wall left
+    dc.b %1100 ; wall top
+    dc.b %1010 ; wall right
+    dc.b %0011 ; wall bottom
+    dc.b %1001 ; flag
+    
